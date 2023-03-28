e43a99bec9cf61ea29955518ec728ccf2ba342c5
Pablo-Laborde <p5.tacoronte@gmail.com>
2023-03-28 10:39:15 -0700

    printf

--- printf.c
+++ printf.c
@@ -5,23 +5,6 @@
  * @format: string input
  * Return: length of string if valid ,else a negative number
  */
-int (*get_func(char c))(buff_t*, va_list)
-{
-	int i = 0;
-
-	buff_t opt[] = {
-		{'c', ADD_CHAR},
-		{'\0', NULL},
-	};
-
-	while (*(opt[i].ops) != *s)
-	{
-		i++;
-	}
-
-	return (ops[i].buffer);
-}
-
 int _printf(const char *format, ...)
 {
 	/* var decalration */
