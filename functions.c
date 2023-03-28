b3872c273fb140ef7649ef27b94e44cf3066356a
Pablo-Laborde <p5.tacoronte@gmail.com>
2023-03-28 11:32:12 -0700

    functions

---
+++ functions.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * ADD_STR- function
+ * @b: input
+ * @ap: input
+ * Return: void
+ */
+void ADD_STR(buff_t *b, va_list ap)
+{
+	/* var declaration */
+	int i;
+	char *s;
+
+	/* code */
+	s = va_arg(ap, char*);
+	if (s != NULL && s != "")
+	{
+		b->buffer[b->pos] = s[0];
+		i = 1;
+		while (s[i] != '\0')
+		{
+			b->pos += 1;
+			if (s[i] == '\')
+			{
+				i++;
+				if (s[i] == 'n')
+					b->buffer[b->pos] = '\n';
+			}
+			else
+				b->buffer[b->pos] = s[i];
+			i++;
+		}
+	}
+}
