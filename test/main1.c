61486aad81cff7d9882ccc00a14cba9a4dc279ac
hentype85 <martinleiro9@gmail.com>
2023-03-29 10:29:28 -0700

    ain1

---
+++ test/main1.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+int main (void)
+{
+	printf("holaaa%%%%aaaa### ");
+	_printf("holaaa%%%%aaaa### ");
+	printf("12345%a%%%%%%\\\\");
+	_printf("12345%a%%%%%%\\\\");
+	printf("%%s%%r%1buelaaaaaas%%s%%%%%%%%%%sali de %k");
+	_printf("%%s%%r%1buelaaaaaas%%s%%%%%%%%%%sali de %k");
+	printf("%c%c%c%c", 'c', 'a', 'c', 'a');
+	_printf("%c%c%c%c", 'c', 'a', 'c', 'a');
+	printf("%%%%c%%c%c", 90, NULL);
+	_printf("%%%%c%%c%c", 90, NULL);
+	
+	return (0);
+}
