3a49e7104b13237c9f62998f1b848390d7c35e64
Pablo-Laborde <p5.tacoronte@gmail.com>
2023-03-29 11:33:42 -0700

    fix

--- load_buffer.c
+++ load_buffer.c
@@ -16,21 +16,37 @@ void load_buffer(buff_t *b, const char *format, int *i, va_list ap)
 
 	/* code */
 	j = *i;
-	while ((format[j] != '\0') && (b->pos < BUFFSIZE))
+	while ((format[j] != '\0')) /* && (b->pos < BUFFSIZE)) */
 	{
 		if (format[j] == '%')
 		{
-			j++;
-			f = get_func(format[j]);
-			if (f != NULL)
-				(*f)(b, ap);
+			if (format[j + 1] == '\0')
+				j++;
 			else
-				b->buffer[b->pos] = format[j];
+			{
+				f = get_func(format[j + 1]);
+				if (f != NULL)
+				{
+					(*f)(b, ap);
+					j++;
+				}
+				else
+				{
+					b->buffer[b->pos] = format[j];
+					j++;
+					b->pos += 1;
+					b->buffer[b->pos] = format[j];
+				}
+			j++;
+			b->pos += 1;
+			}
 		}
 		else
+		{
 			b->buffer[b->pos] = format[j];
-		j++;
-		b->pos += 1;
+			j++;
+			b->pos += 1;
+		}
 	}
 	*i = j;
 }
--- test/main1.c
+++ test/main1.c
@@ -3,16 +3,16 @@
 
 int main (void)
 {
-	printf("holaaa%%%%aaaa### ");
-	_printf("holaaa%%%%aaaa### ");
-	printf("12345%a%%%%%%\\\\");
-	_printf("12345%a%%%%%%\\\\");
-	printf("%%s%%r%1buelaaaaaas%%s%%%%%%%%%%sali de %k");
-	_printf("%%s%%r%1buelaaaaaas%%s%%%%%%%%%%sali de %k");
-	printf("%c%c%c%c", 'c', 'a', 'c', 'a');
-	_printf("%c%c%c%c", 'c', 'a', 'c', 'a');
-	printf("%%%%c%%c%c", 90, NULL);
-	_printf("%%%%c%%c%c", 90, NULL);
+printf("holaaa%%%%aaaa### ");
+_printf("holaaa%%%%aaaa### ");
+printf("12345%a%%%%%%\\\\");
+_printf("12345%a%%%%%%\\\\");
+printf("%%s%%r%1buelaaaaaas%%s%%%%%%%%%%sali de %k");
+_printf("%%s%%r%1buelaaaaaas%%s%%%%%%%%%%sali de %k");
+printf("%c%c%c%c", 'c', 'a', 'c', 'a');
+_printf("%c%c%c%c", 'c', 'a', 'c', 'a');
+printf("%%%%c%%c%c", 90, NULL);
+_printf("%%%%c%%c%c", 90, NULL);
 	
 	return (0);
 }
