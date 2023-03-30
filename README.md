71d5dda7c682ef01af0efc1f98fcd8dce31f1960
Pablo-Laborde <p5.tacoronte@gmail.com>
2023-03-30 10:47:13 -0700

    a

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 printf README
 
-martin probando 
+martin probando
---
+++ func1.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * conv_to_binary- function
+ * @b: input
+ * @ap: input
+ * Return: void
+ */
+void conv_to_binary(buff_t *b, va_list ap)
+{
+	/* var declaration */
+	int i;
+	unsigned int num;
+	char number[32];
+
+	/* code */
+	num = va_arg(ap, unsigned int);
+	for (i = 31; i >= 0; i--)
+	{
+		number[i] = num % 2;
+		num /= 2;
+	}
+	i = 0;
+	while (number[i] != '1')
+		i++;
+	while (i < 32)
+	{
+		b->buffer[b->pos] = number[i];
+		i++;
+		b->pos++;
+		if (b->pos == BUFFSIZE)
+			buff_ctrl(b);
+	}
+}
